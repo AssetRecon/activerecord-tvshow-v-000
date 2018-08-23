f68e18ea1801f5511123c82c31677fef6c540749
Edsel Caprice <caprice@mail.usf.edu>
2018-08-23 02:49:53 +0000

    automatically backed up by learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 class AddSeasonToShows < ActiveRecord::Migration
 
   def change
-=======
-class AddSeasonToShows < ActiveRecord::Migration[5.2]
 
   def self.change
 >>>>>>> a0829f780a238f25734e3013a738f2317e6c0224
